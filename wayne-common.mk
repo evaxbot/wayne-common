0f51601b3bc9c0783951b94fc25430473efaa2e7
evaxbot <harshavarthan.0309@gmail.com>
2021-08-03 00:36:10 +0530

    initial caf

--- wayne-common.mk
+++ wayne-common.mk
@@ -69,8 +69,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay 
 
 # Permissions
 PRODUCT_COPY_FILES += \
